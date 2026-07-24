c7d53a20ec1845907585b7c260715cd329681ac7
Caleb Maclennan <caleb@alerque.com>
2026-07-24 21:11:56 +0300

    Add horizontal scrolling gesture

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -203,6 +203,7 @@ hl.bind("SUPER + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind("SUPER + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 hl.gesture({ fingers = 3, direction = "vertical", action = "workspace" })
+hl.gesture({ fingers = 3, direction = "horizontal", action = "scroll_move" })
 hl.gesture({
    fingers = 4,
    direction = "vertical",
